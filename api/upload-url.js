f26ec932480bb4a2a5f6839e4ced7050b7de2a2b
Lu_Shiyi <lu_shiyi@sjtu.edu.cn>
2026-04-05 11:36:52 +0800

    Fix formatting in upload-url.js

--- api/upload-url.js
+++ api/upload-url.js
@@ -3,10 +3,10 @@ export default async function handler(req, res) {
   const folder = `capsules/${new Date().toISOString().slice(0,7)}`
   
   res.json({
-    url: `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/auto/upload`,
+    url: `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/auto/upload`,  // 删除空格
     params: {
-      upload_preset: 'time_capsule',  // 你创建的 preset 名称
-      folder  // 可选，覆盖 preset 中的 folder
+      upload_preset: 'time_capsule',
+      folder
     }
   })
 }
